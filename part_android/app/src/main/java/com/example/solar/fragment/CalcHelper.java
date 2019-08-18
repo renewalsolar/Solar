e7fc2149bf2f7ba769fe1facde5fff9ca9228ca8
yunburger <50167146+yunburger@users.noreply.github.com>
2019-08-19 03:12:33 +0900

    Complete

--- part_android/app/src/main/java/com/example/solar/fragment/CalcHelper.java
+++ part_android/app/src/main/java/com/example/solar/fragment/CalcHelper.java
@@ -1,5 +1,6 @@
 package com.example.solar.fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
@@ -11,10 +12,13 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import com.example.solar.R;
+import com.github.mikephil.charting.data.*;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class CalcHelper extends Fragment {
 
@@ -26,6 +30,8 @@ public class CalcHelper extends Fragment {
     TextView text_won;
     TextView text_end;
     ImageView img_nu;
+    LineChart lineChart;
+
     public CalcHelper() {
         // Required empty public constructor
     }
@@ -45,6 +51,7 @@ public class CalcHelper extends Fragment {
         text_bill = (TextView)v.findViewById(R.id.bill);
         text_won = (TextView)v.findViewById(R.id.won);
         text_end = (TextView)v.findViewById(R.id.end);
+        lineChart = (LineChart)v.findViewById(R.id.linechart);
 
         pwr_qty = 472.2;
         money = (300 * 93.3) + (150 * 187.9) + (22.2 * 280.6);
@@ -63,6 +70,25 @@ public class CalcHelper extends Fragment {
         text_bill.setText("편상진 님의 7월 청구금액은");
         text_won.setText(Double.toString(money) + "원");
         text_end.setText("입니다.");
+
+        lineChart.invalidate(); //차트 초기화 작업
+        lineChart.clear();
+
+        ArrayList<Entry> values = new ArrayList<>();//차트 데이터 셋에 담겨질 데이터
+
+        values.add(new Entry(3, 35000));
+        values.add(new Entry(4, 27850));
+        values.add(new Entry(5, 42650));
+        values.add(new Entry(6, 41550));
+
+        /*몸무게*/
+        LineDataSet lineDataSet = new LineDataSet(values, "요금"); //LineDataSet 선언
+
+        LineData lineData = new LineData(); //LineDataSet을 담는 그릇 여러개의 라인 데이터가 들어갈 수 있습니다.
+        lineData.addDataSet(lineDataSet);
+
+        lineData.setValueTextSize(9);
+        lineChart.setData(lineData);
 
         return v;
     }
