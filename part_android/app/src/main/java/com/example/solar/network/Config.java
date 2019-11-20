708715e5994c09d538bc0bb18a3522efbc44d5f1
JaeUkas <anytime96@naver.com>
2019-11-20 21:20:47 +0900

    fix map

--- part_android/app/src/main/java/com/example/solar/network/Config.java
+++ part_android/app/src/main/java/com/example/solar/network/Config.java
@@ -1,7 +1,7 @@
 package com.example.solar.network;
 
 public class Config {
-    public final static String MAIN_URL = "http://192.168.212.210:3001/";
+    public final static String MAIN_URL = "http://192.168.210.29:3001/";
 
     public final static String POST_REGISTER = "api/person/register/";
     public final static String POST_LOGIN = "api/person/login/";
